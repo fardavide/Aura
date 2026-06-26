ba371874964bea41ea21eca80071aff5c6f54fdb
Davide Farella <davide@farella.info>
2026-06-26 14:42:02 +0200

    Initial Commit

---
+++ AuraTests/AuraTests.swift
@@ -0,0 +1,13 @@
+//
+
+import Testing
+
+struct AuraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
